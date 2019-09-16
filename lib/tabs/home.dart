5f8b6856e51b5da39374afd6c148ff880cbd79bb
Maksym Karasha <MaxKarasha@gmail.com>
2019-09-17 00:26:46 +0300

    Кнопка добавления и демосписок.

--- lib/tabs/home.dart
+++ lib/tabs/home.dart
@@ -1,28 +1,43 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.red,
-      body: Container(
-        child: Center(
-          child: Column(
-            // center the children
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Icon(
-                Icons.group_work,
-                size: 160.0,
-                color: Colors.white,
-              ),
-              Text(
-                "Home",
-                style: TextStyle(color: Colors.white),
-              )
-            ],
-          ),
+    return PausedList(
+      items: List<String>.generate(10, (i) => "Item $i"),
+    );
+  }
+}
+
+class PausedList extends StatelessWidget {
+  final List<String> items;
+
+  PausedList({Key key, @required this.items}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Long List';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
         ),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text('${items[index]}'),
+            );
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        )
       ),
     );
   }
